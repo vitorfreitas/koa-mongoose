2aae9c11e539e958f90ebc2704d9f9cbeae8c585
Vitor Freitas <vitorldfreitas@gmail.com>
2019-06-24 19:07:31 -0300

    add test environment with .env.test

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
-require('dotenv/config');
+require('dotenv').config({
+  path: process.env.NODE_ENV === 'test' ? '.env.test' : '.env'
+});
 
 const app = require('./src/app');
 const port = process.env.PORT || 3000;
--- src/app.js
+++ src/app.js
@@ -1,4 +1,7 @@
-require('dotenv/config');
+require('dotenv').config({
+  path: process.env.NODE_ENV === 'test' ? '.env.test' : '.env'
+});
+
 const Koa = require('koa');
 const bodyParser = require('koa-body');
 
